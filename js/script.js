a4c87df1f7a4cfbfacd7e0beb515b30f1527ffc6
mjwfilippo <mjwfilippo@gmail.com>
2024-05-24 13:59:37 +0200

    Several fixes

--- js/script.js
+++ js/script.js
@@ -1,115 +1,115 @@
-const currentdate = new Date();
-let weekday = currentdate.getDay();
-let monthday = currentdate.getDate();
-let month = currentdate.getMonth() + 1;
-let year = currentdate.getFullYear();
+const currentDate = new Date();
+let weekDay = currentDate.getDay();
+let monthDay = currentDate.getDate();
+let month = currentDate.getMonth() + 1;
+let year = currentDate.getFullYear();
 
-switch (weekday) {
+switch (weekDay) {
     case 1:
-        weekday = "maandag";
+        weekDay = 'maandag';
         break;
     case 2:
-        weekday = "dinsdag";
+        weekDay = 'dinsdag';
         break;
     case 3:
-        weekday = "woensdag";
+        weekDay = 'woensdag';
         break;
     case 4:
-        weekday = "donderdag";
+        weekDay = 'donderdag';
         break;
     case 5:
-        weekday = "vrijdag";
+        weekDay = 'vrijdag';
         break;
     case 6:
-        weekday = "zaterdag";
+        weekDay = 'zaterdag';
         break;
     case 7:
-        weekday = "zondag";
+        weekDay = 'zondag';
         break;
     default:
-        weekday = "onbekend";
+        weekDay = 'onbekend';
 }
 
 switch (month) {
     case 1:
-        month = "januari";
+        month = 'januari';
         break;
     case 2:
-        month = "februari";
+        month = 'februari';
         break;
     case 3:
-        month = "maart";
+        month = 'maart';
         break;
     case 4:
-        month = "april";
+        month = 'april';
         break;
     case 5:
-        month = "mei";
+        month = 'mei';
         break;
     case 6:
-        month = "juni";
+        month = 'juni';
         break;
     case 7:
-        month = "juli";
+        month = 'juli';
         break;
     case 8:
-        month = "augustus";
+        month = 'augustus';
         break;
     case 9:
-        month = "september";
+        month = 'september';
         break;
     case 10:
-        month = "oktober";
+        month = 'oktober';
         break;
     case 11:
-        month = "november";
+        month = 'november';
         break;
     case 12:
-        month = "december";
+        month = 'december';
         break;
     default:
-        month = "onbekend";
+        month = 'onbekend';
 }
 
-const dateString = `${weekday}, ${monthday} ${month} ${year}`;
-$("#datum").text(dateString);
+const dateString = `${weekDay}, ${monthDay} ${month} ${year}`;
+$('#datum').text(dateString);
 
-$("button").click(function () {
-    $(".spoiler-text").slideToggle(2000);
-    if ($(this).text() == "Verbergen") {
-        $(this).text("Terughalen");
+$('button').click(function () {
+    $('.spoiler-text').slideToggle(500);
+    if ($(this).text() === 'Verbergen') {
+        $(this).text('Terughalen');
     } else {
-        $(this).text("Verbergen");
+        $(this).text('Verbergen');
     }
 });
 
-$("img").click(function () {
-    $(this).toggleClass("altimage");
+$('img').click(function () {
+    $(this).toggleClass('alt-image');
 });
 
-$(".icon").click(function () {
-    if (confirm("Weet je het zeker?\nJe krijgt hier mogelijk spijt van.")) {
-        $("body").addClass("aaaaah");
-        $("img").attr("src", "./images/nicolas-cage.jpg");
-        $("a").attr("href", "./images/wallpaper.jpg");
-        $("#datum").text("tijd voor Nicolas Cage");
-        $(".list-info").text("Beste films ooit:");
-        $("li:nth-of-type(1)").text("Face/Off");
-        $("li:nth-of-type(2)").text("Vampire's Kiss");
-        $("li:nth-of-type(3)").text("Ghost Rider");
-        $("li:nth-of-type(4)").text("Honeymoon in Vegas");
-        $("li:nth-of-type(5)").text("National Treasure");
-        $("li:nth-of-type(6)").text("Con Air");
-        $("li:nth-of-type(7)").text("Willy's Wonderland");
-        $("li:nth-of-type(8)").text("The Wicker Man");
-        $("li:nth-of-type(9)").text("Moonstruck");
-        $("li:nth-of-type(10)").text("Color Out of Space");
-        $(".icon").text("🤡");
-        $(".icon").off("click");
+$('.icon').click(function () {
+    if (confirm('Weet je het zeker?\nJe krijgt hier mogelijk spijt van.')) {
+        $('body').addClass('aaaaah');
+        $('img').attr('src', './images/nicolas-cage.jpg');
+        $('a').attr('href', './images/wallpaper.jpg');
+        $('#datum').text('tijd voor Nicolas Cage');
+        $('.list-info').text('Beste films ooit:');
+        $('li:nth-of-type(1)').text('Face/Off');
+        $('li:nth-of-type(2)').text('Vampire\'s Kiss');
+        $('li:nth-of-type(3)').text('Ghost Rider');
+        $('li:nth-of-type(4)').text('Honeymoon in Vegas');
+        $('li:nth-of-type(5)').text('National Treasure');
+        $('li:nth-of-type(6)').text('Con Air');
+        $('li:nth-of-type(7)').text('Willy\'s Wonderland');
+        $('li:nth-of-type(8)').text('The Wicker Man');
+        $('li:nth-of-type(9)').text('Moonstruck');
+        $('li:nth-of-type(10)').text('Color Out of Space');
+        $('.icon').text('🤡').off('click');
+        $('.name').text('Nick Cage');
     }
 });
 
-$(".name").click(function () {
-    const name = prompt("Wat is je naam", "Nick Cage");
+$('.name').click(function () {
+    const name = prompt('Wat is je naam', 'Nick Cage');
     $(this).text(name);
 });
